42c3e00d9b5f3471d5fd08202de0868417d12af2
Kohei.Morita <kanitako@gmail.com>
2022-02-07 20:38:59 +0900

    111a

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,28 @@
+var windowwidth = window.innerWidth || document.documentElement.clientWidth || 0;
+    if (windowwidth > 768){
+      var responsiveImage = [
+        { src: '../images/spring/haru.jpg'},
+        { src: '../images/summer/summer.jpg'},
+        { src: '../images/autumn/fall.jpg' },
+        { src: '../images/winter/fuyu.jpg'}
+      ];
+    } else {
+      var responsiveImage = [
+        { src: '../images/spring/haru.jpg'},
+        { src: '../images/summer/summer.jpg'},
+        { src: '../images/autumn/fall.jpg' },
+        { src: '../images/winter/fuyu.jpg'}
+      ];
+}
 
+$('#top').vegas({
+  overlay: true,
+  transition: 'blur',
+  transitionDuration: 2000,
+  delay: 10000,
+  animationDuration: 20000,
+  animation: 'kenburns',
+  slides:responsiveImage,
+});
 
 
