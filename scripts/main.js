aa29b7662ce5c8e4f3220d0a12e9c6b6ac977cdc
Kohei.Morita <kanitako@gmail.com>
2022-02-09 06:37:34 +0900

    111a

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,14 @@
 var windowwidth = window.innerWidth || document.documentElement.clientWidth || 0;
     if (windowwidth > 768){
       var responsiveImage = [
-        { src: '../images/spring/haru.jpg'},
+        { src: '../images/spring/sakura.jpg'},
         { src: '../images/summer/summer.jpg'},
         { src: '../images/autumn/fall.jpg' },
         { src: '../images/winter/fuyu.jpg'}
       ];
     } else {
       var responsiveImage = [
-        { src: '../images/spring/haru.jpg'},
+        { src: '../images/spring/sakura.jpg'},
         { src: '../images/summer/summer.jpg'},
         { src: '../images/autumn/fall.jpg' },
         { src: '../images/winter/fuyu.jpg'}
@@ -19,10 +19,26 @@ $('#top').vegas({
   overlay: true,
   transition: 'blur',
   transitionDuration: 2000,
-  delay: 10000,
+  delay: 3000,
   animationDuration: 20000,
   animation: 'kenburns',
   slides:responsiveImage,
 });
 
-
+$(function(){
+	$(window).on('load scroll',function (){
+		$('.animation').each(function(){
+			//ターゲットの位置を取得
+			var target = $(this).offset().top;
+			//スクロール量を取得
+			var scroll = $(window).scrollTop();
+			//ウィンドウの高さを取得
+			var height = $(window).height();
+			//ターゲットまでスクロールするとフェードインする
+			if (scroll > target - height){
+				//クラスを付与
+				$(this).addClass('active');
+			}
+		});
+	});
+});
