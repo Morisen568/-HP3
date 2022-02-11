69574264480431f06d22c3fd7734de83c53d0af8
Kohei.Morita <kanitako@gmail.com>
2022-02-11 20:41:11 +0900

    111a

--- scripts/main.js
+++ scripts/main.js
@@ -28,17 +28,45 @@ $('#top').vegas({
 $(function(){
 	$(window).on('load scroll',function (){
 		$('.animation').each(function(){
-			//ターゲットの位置を取得
 			var target = $(this).offset().top;
-			//スクロール量を取得
 			var scroll = $(window).scrollTop();
-			//ウィンドウの高さを取得
 			var height = $(window).height();
-			//ターゲットまでスクロールするとフェードインする
 			if (scroll > target - height){
-				//クラスを付与
 				$(this).addClass('active');
 			}
 		});
 	});
 });
+
+	$('.slider').slick({
+		autoplay: true,
+		infinite: true,
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		prevArrow: '<div class="slick-prev"></div>',
+		nextArrow: '<div class="slick-next"></div>',
+		dots: true,
+    responsive: [
+      {
+			breakpoint: 1700,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3,
+			}
+      },
+      {
+			breakpoint: 1300,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2,
+			}
+		},
+			{
+			breakpoint: 769,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+			}
+		},
+	]
+	});
